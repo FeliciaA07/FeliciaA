b55c93976a9178f3d23eb6474a88aae80c04f991
Felicia Aguiar <feliciaaguiar@ORRHS.ORG>
2020-02-10 13:56:13 -0500

    banana game

---
+++ Clicker Game/app.js
@@ -0,0 +1,15 @@
+let bananaImg
+let heading
+
+function setup () {
+    createCanvas(windowWidth, windowHeight)
+    bananaImg = createImg('banana.png', 'banana image')
+    bananaImg.size(200, 100)
+    heading = createElement('h2', 'Click the Banana!')
+    heading.position(width / 3, height / 4)
+}
+
+function draw() {
+    bananaImg.position(random(width), random(height))
+    bananaImg.mousePressed(youWon)
+}
