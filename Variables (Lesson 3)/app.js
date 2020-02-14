2031a77b0f96261765e13ed851986abe4a541ac5
Felicia Aguiar <feliciaaguiar@ORRHS.ORG>
2020-02-14 09:39:39 -0500

    I tried 2/14

--- Variables (Lesson 3)/app.js
+++ Variables (Lesson 3)/app.js
@@ -1,9 +1,12 @@
+// const add = 7 + 3;
+// const sub = 7 - 3;
+// const mult = 7 * 3;
+// const div = 7 / 3;
+// const paren = (83 + 92 + 86 + 94) / 4;
+// console.log(add, sub, mult, div, paren)
+
 function setup(){
-const add = 7 + 3;
-const sub = 7 - 3;
-const mult = 7 * 3;
-const div = 7 / 3;
-const paren = (83 + 92 + 86 + 94) / 4;
+
 }
 
 // let score = 0
@@ -11,3 +14,25 @@ const paren = (83 + 92 + 86 + 94) / 4;
 
 // let numLikes = 7
 // console.log('So far ' + numLikes + ' people like this page')
+
+// let score = 0;
+// let lives = 3;
+// console.log('You have ' + lives + ' lives left with score: ' + score);
+
+// let points = 1023;
+// let lives = 3;
+// let totalTime = 45;
+// const paren = (1023 * 3) / 45;
+// console.log("Points: " + points);
+// console.log("Lives: " + lives);
+// console.log("Total Time: " + totalTime + " secs");
+// console.log("---------------------------");
+// // let totalScore = console.log(paren);
+// console.log("Your Total Score is: " + console.log(paren));
+
+// let a = 3
+// let b = 7
+// a = b
+// b = a
+// console.log(a,b)
+
